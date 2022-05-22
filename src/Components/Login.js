4b86b8fb0c0522b95d5d8b1c28d3b6b3b16130c2
Shital Nagpure <bhartinagpure2409@gmail.com>
2022-05-23 03:38:35 +0900

    modified

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,12 +1,9 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import './Login.css'
-import { logOut ,signIn} from '../Firebase'
-import NavHome from './NavHome'
+import { logOut } from '../Firebase'
 
 function Login() {
-  const emailRef = useRef();
-  const passRef = useRef();
 
   const handleLogOut = async() => {
     try{
@@ -16,30 +13,19 @@ function Login() {
       alert(error.message);
     }
   }
-
-  const handleSignIn = async() => {
-    try{
-      await signIn(emailRef.current.value , passRef.current.value);
-    }
-    catch(e){
-      alert(e.message)
-    }
-  }
   return (
     <div >
-      <NavHome/>
         <center>
-        
-            <div id='main-div'> 
-            
+          
+            <div id='main-div'>
             <section className='input'>
                 <div className="input-group mb-3 ">
-                  <input ref={emailRef} type="email" className="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1"/>
+                  <input type="email" className="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1"/>
                </div>
             </section>
             <section className='input'>
             <div className="input-group mb-3 ">
-                <input ref={passRef} type="password" className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1"/>
+                <input type="password" className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1"/>
             </div>
             </section>
 
@@ -47,16 +33,12 @@ function Login() {
                 <Link to="/forgotpassword">Forgot Password?</Link>
             </div>
 
-            <div>
-              Not a member? <Link to='/signup'>SignUp</Link>
-            </div>
-
-            <button type="button" onClick={handleSignIn} className="btn btn-info">Log In</button>
+            <button type="button" className="btn btn-info">Log In</button>
 
             <button type="button" onClick={handleLogOut} className="btn btn-info">Log out</button>
             </div>
         </center>
-    </div> 
+    </div>
   )
 }
 
--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -1,29 +1,54 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Interaction from '../Images/Interaction.png';
 import Navbar from './Navbar';
-import './Welcome.css';
-import { useAuth } from '../Firebase';
-function Welcome() {
-    const currentUser = useAuth();
-   
-  return (
-      <>
-      <Navbar/>
-      
-      <div id='main-div'>
+import './Welcome.css'
+
+
+function Home () {
+    // const myStyle ={
+    //     image:{
+    //         width:'600px',
+    //         height:'370px',
+    //         marginTop:'50px',
+    //         marginRight:'-117px'
+    //     },
+    //     text:{
+    //         fontSize:'63px',
+    //         fontWeight:'600',
+    //         color:'#4f94d4',
+    //         textOverflow:'clip'
+    //     },
+    //     subtext:{
+    //         fontSize:'18px',
+    //         marginTop:'10px',
+    //         marginBottom:'38px'
+    //     },
+    //     button:{
+    //         border:'none',
+    //         backgroundColor:'#135e96',
+    //         color:'white',
+    //         padding:'16px 38px',
+    //         fontSize:'16px',
+    //         borderRadius:'10px',
+    //         cursor:'pointer'
+    //     }
+    // }
+    return (
+        <>
+        <Navbar/>
+        <div id='main-div'>
         <div style={{width:'750px'}} > 
-            <h3 id='text' > Introducing Interaction Portal of TGPCET!!</h3>
-            <p id='subtext'>Search for the Alumni respective to their departments and reach out to them , also get updates of upcoming Placement Drives.</p>
-            <Link to="dashboard"><button className='btn' disabled={currentUser ? false : true}>Get Started</button></Link>
+            <h3 id="text">Introducing Interaction Portal of TGPCET!!</h3>
+            <p id="subtext" >Search for the Alumni respective to their departments and reach out to them , also get updates of upcoming Placement Drives.</p>
+            <Link to="/dashboard">< button className='btn' >Get Started</button></Link>
         </div >
         <div>
-            <img id='img' src={Interaction} alt="interaction-img" />
+            <img id="img"  src={Interaction} alt="interaction-img" />
         </div>
     </div>
-    </>
-
-  )
+        </>
+    )
 }
 
-export default Welcome
+export default Home;
